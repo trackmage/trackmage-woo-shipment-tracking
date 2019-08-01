8f7dbca0c852542f6e1a7f99d0a8b45e95294668
Mahmoud Saeed <msaeed.ma@gmail.com>
2019-08-01 19:58:07 +0200

    Create status management table

--- templates/admin/settings-statuses.php
+++ templates/admin/settings-statuses.php
@@ -10,6 +10,34 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
+
+// Get the registered statuses.
+$statuses = wc_get_order_statuses();
 ?>
 
-Statues
+<div class="intro"><?php _e( 'You can edit and add new order statuses.', 'trackmage' ); ?></div>
+
+<table class="widefat status-manager" id="statusManager">
+	<thead>
+		<tr>
+		<th><?php _e( 'Name', 'trackmage' ); ?></th>
+		<th><?php _e( 'Slug', 'trackmage' ); ?></th>
+		<th><?php _e( 'Alias in TrackMage', 'trackmage' ); ?></th>
+		<th><?php _e( 'Actions', 'trackmage' ); ?></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach( $statuses as $slug => $name ) : ?>
+		<tr>
+			<td><?php echo $name; ?></td>
+			<td><?php echo $slug; ?></td>
+			<td></td>
+			<td><a href="">Edit</a></td>
+		</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
+
+<form>
+	<input type="submit" class="button button-primary" value="<?php _e( 'Add New Status', 'trackmage' ); ?>" />
+</form>
--- templates/admin/settings.php
+++ templates/admin/settings.php
@@ -21,15 +21,13 @@ settings_errors();
 ?>
 <div class="wrap trackmage">
 	<h1><?php _e( 'TrackMage Settings', 'trackmage' ); ?></h1>
-	<form method="post" action="options.php" id="trackmage-settings-general">
-		<nav class="nav-tab-wrapper trackmage-nav-tab-wrapper">
-			<a href="<?php echo admin_url( 'admin.php?page=trackmage&amp;tab=general' ); ?>" class="nav-tab<?php Utils::add_css_class( 'general' === $tab || '' === $tab, 'nav-tab-active', true, true ); ?>"><?php _e( 'General', 'trackmage' ); ?></a>
-			<a href="<?php echo admin_url( 'admin.php?page=trackmage&amp;tab=statuses' ); ?>" class="nav-tab<?php Utils::add_css_class( 'statuses' === $tab, 'nav-tab-active', true, true ); ?>"><?php _e( 'Statuses', 'trackmage' ); ?></a>
-		</nav>
-		<div class="postbox">
-			<div class="inside tab tab-<?php echo $tab; ?>">
-				<?php include( TRACKMAGE_DIR . "templates/admin/settings-{$tab}.php" ); ?>
-			</div>
+	<nav class="nav-tab-wrapper trackmage-nav-tab-wrapper">
+		<a href="<?php echo admin_url( 'admin.php?page=trackmage&amp;tab=general' ); ?>" class="nav-tab<?php Utils::add_css_class( 'general' === $tab || '' === $tab, 'nav-tab-active', true, true ); ?>"><?php _e( 'General', 'trackmage' ); ?></a>
+		<a href="<?php echo admin_url( 'admin.php?page=trackmage&amp;tab=statuses' ); ?>" class="nav-tab<?php Utils::add_css_class( 'statuses' === $tab, 'nav-tab-active', true, true ); ?>"><?php _e( 'Statuses', 'trackmage' ); ?></a>
+	</nav>
+	<div class="postbox">
+		<div class="inside tab tab-<?php echo $tab; ?>">
+			<?php include( TRACKMAGE_DIR . "templates/admin/settings-{$tab}.php" ); ?>
 		</div>
-	</form>
+	</div>
 </div>
