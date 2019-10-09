85f84897cefa4828f0e706b7de8221293fa31a9f
harbuzniak <fnostric@gmail.com>
2019-10-09 15:12:54 +0300

    Use TM statuses in Alias

--- src/Helper.php
+++ src/Helper.php
@@ -46,7 +46,7 @@ class Helper {
 
             return 2;
         }
-        
+
         return 1;
     }
 
@@ -114,10 +114,26 @@ class Helper {
      * @return array List of aliases.
      */
     public static function get_aliases() {
-        return [
-            'delivered' => __( 'Delivered', 'trackmage' ),
-            'shipped'   => __( 'Shipped', 'trackamge' ),
-        ];
+        $workspaceId = get_option( 'trackmage_workspace', 0 );
+        if(!$workspaceId)
+            return [];
+
+        $aliases = [];
+        try {
+            $client = Plugin::get_client();
+            $guzzleClient = $client->getGuzzleClient();
+            $response = $guzzleClient->get("/workspaces/{$workspaceId}/order_statuses");
+            $content = $response->getBody()->getContents();
+            $data = json_decode($content, true);
+        } catch( ApiException $e ) {
+            // Do nothing. We will return an empty array.
+        }
+        foreach ($data['hydra:member'] as $status) {
+            $name = $status['name'];
+            $aliases[$name] = __(ucfirst($name),'trackmage');
+        }
+
+        return $aliases;
     }
 
     /**
@@ -348,9 +364,9 @@ class Helper {
     public static function getallheaders() {
         $headers = array();
 
-        foreach ( $_SERVER as $name => $value ) { 
-            if ( substr( $name, 0, 5 ) == 'HTTP_' ) { 
-                $headers[ str_replace( ' ', '-', ucwords( strtolower( str_replace( '_', ' ', substr( $name, 5 ) ) ) ) ) ] = $value; 
+        foreach ( $_SERVER as $name => $value ) {
+            if ( substr( $name, 0, 5 ) == 'HTTP_' ) {
+                $headers[ str_replace( ' ', '-', ucwords( strtolower( str_replace( '_', ' ', substr( $name, 5 ) ) ) ) ) ] = $value;
             }
         }
 
@@ -380,7 +396,7 @@ class Helper {
     public static function add_css_class( $condition = false, $class = '', $leading_space = false, $echo = false ) {
         if ( $condition ) {
             $output = ( $leading_space ? ' ' : '' ) . $class;
-            
+
             if ( $echo ) {
                 echo $output;
             } else {
--- views/admin-page-status-manager.php
+++ views/admin-page-status-manager.php
@@ -13,13 +13,13 @@ if ( ! defined( 'WPINC' ) ) {
 
 use TrackMage\WordPress\Helper;
 
-$sync_statuses = get_option( 'trackmage_sync_statuses', [] );
-
 // Get the registered statuses.
 $statuses = Helper::getOrderStatuses();
 
 // Get the aliases.
 $aliases = Helper::get_aliases();
+
+$used_aliases = [];
 ?>
 <div class="wrap trackmage">
     <h1><?php _e( 'Status Manager', 'trackmage' ); ?></h1>
@@ -47,7 +47,7 @@ $aliases = Helper::get_aliases();
                         </div>
                     </td>
                     <td><span data-update-status-slug><?php echo $slug; ?></span></td>
-                    <td colspan="2"><span data-update-status-alias><?php echo isset( $aliases[ $status['alias'] ] ) ? $aliases[ $status['alias'] ] : ''; ?></span></td>
+                    <td colspan="2"><span data-update-status-alias><?php echo isset( $aliases[ $status['alias'] ] ) ? $used_aliases[$status['alias']] = $aliases[ $status['alias'] ] : ''; ?></span></td>
                 </tr>
                 <?php endforeach; ?>
             </tbody>
@@ -59,7 +59,7 @@ $aliases = Helper::get_aliases();
                         <select name="status_alias">
                             <option value=""><?php _e( '— Select —', 'trackmage' ); ?></option>
                             <?php foreach ( $aliases as $id => $name ) : ?>
-                                <option value="<?php echo $id; ?>"><?php echo $name; ?></option>
+                                <option value="<?php echo $id; ?>" <?php echo isset($used_aliases[$id])?'style="display: none"':'';?>><?php echo $name; ?></option>
                             <?php endforeach; ?>
                         </select>
                     </td>
