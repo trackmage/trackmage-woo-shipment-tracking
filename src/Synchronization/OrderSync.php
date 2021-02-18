4682451181eb0d13089457889c18cb2d9d178770
evghenii <harbuzniak.yevhenii@gmail.com>
2021-02-18 10:58:10 +0200

    Fix TM-2260 synchronization process

--- src/Synchronization/OrderSync.php
+++ src/Synchronization/OrderSync.php
@@ -3,7 +3,6 @@
 namespace TrackMage\WordPress\Synchronization;
 
 use GuzzleHttp\Exception\ClientException;
-use phpDocumentor\Reflection\Types\Boolean;
 use Psr\Http\Message\ResponseInterface;
 use TrackMage\WordPress\Exception\SynchronizationException;
 use TrackMage\WordPress\Helper;
